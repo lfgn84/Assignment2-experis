8f1a37ff023555bbaf4c5e863f7eef313d701f00
Luis Gutierrez <luis.gutierrez@se.experis.com>
2021-07-19 08:11:26 +0200

    First Commit

---
+++ src/main/java/com/example/assignment2experis/Assignment2ExperisApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment2experis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2ExperisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2ExperisApplication.class, args);
+    }
+
+}
