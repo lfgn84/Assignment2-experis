1812cb5cc9b35ffb7c6dbda9804e17a088d5c750
kikkipedia <kristinakerlund@gmail.com>
2021-07-21 14:37:28 +0200

    removed old search class

--- src/main/java/se/experis/assignment2experis/controller/SearchController.java
+++ src/main/java/se/experis/assignment2experis/controller/SearchController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import se.experis.assignment2experis.models.ViewSearch;
 import se.experis.assignment2experis.service.ViewService;
 
 
