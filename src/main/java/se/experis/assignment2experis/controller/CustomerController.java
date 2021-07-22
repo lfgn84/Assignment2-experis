b6487d45ecc21ab9e969ae441b1f211cc4f82273
kikkipedia <kristinakerlund@gmail.com>
2021-07-22 14:09:19 +0200

    added comments in CustomerController

--- src/main/java/se/experis/assignment2experis/controller/CustomerController.java
+++ src/main/java/se/experis/assignment2experis/controller/CustomerController.java
@@ -20,48 +20,109 @@ public class CustomerController {
     CustomerService service = new CustomerService();
     Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
+    /**
+     * Gets Customer object by id
+     * @param id
+     * @return Customer object
+     */
     @GetMapping("/{id}")
     public Customer getCustomerById(@PathVariable int id){
         return  service.getCustomerById(id);
     }
 
+    /**
+     * Get all Customer objects
+     * @return an array list of all Customer objects
+     */
     @GetMapping("/all")
     public ArrayList<Customer> getAllCustomers(){
         return service.getAllCustomers();
     }
+    /**
+     *
+     * @param name
+     * @return an array list of Customer objects where FirstName or LastName matches parameter exactly
+     */
     @GetMapping("/selectCustomerByName/{name}")
     public ArrayList<Customer> selectCustomerBySpecificName(@PathVariable String name){
         return service.selectCustomerByName(name);
     }
 
+    /**
+     *
+     * @param name
+     * @return an ArrayList of Customer objects where FirstName or LastName matches parameter in any way
+     */
     @GetMapping("/selectCustomersLike/{name}")
     public ArrayList<Customer> selectCustomersLike(@PathVariable String name){
         return service.selectCustomersLike(name);
     }
+
+    /**
+     *
+     * @param limit how large the ArrayList can be
+     * @param offset the index where the ArrayList will start
+     * @return an ArrayList of Customer objects
+     */
     @GetMapping("/page/{limit},{offset}")
     public ArrayList<Customer> pageCustomers(@PathVariable int limit, @PathVariable int offset){
         return service.pageCustomers(limit, offset);
     }
+
+    /**
+     *
+     * @param customer
+     * @return Boolean if adding a Customer object has been successful or not
+     */
     @PostMapping("/add")
     public Boolean addCustomer(@RequestBody Customer customer){
         return service.addCustomer(customer);
     }
+
+    /**
+     *
+     * @param id Customer id
+     * @param customer Customer object
+     * @return Boolean if updating Customer object has been successful or not
+     */
     @PutMapping("/{id}")
     public Boolean updateCustomer(@PathVariable int id, @RequestBody Customer customer){
         return service.updateCustomer(customer);
     }
+
+    /**
+     *
+     * @return ArrayList of number of Customer objects in each country in descending order
+     */
     @GetMapping("/country")
     public ArrayList<CustomerCountry> countryAmountInCustomers(){
         return service.CountryAmountInCustomers();
     }
+
+    /**
+     *
+     * @return ArrayList of CustomerSpenders ordered by the highest spenders
+     */
     @GetMapping("/spenders")
     public ArrayList<CustomerSpender> highestSpendersInCustomers(){
         return service.HighestSpendersInCustomers();
     }
+
+    /**
+     *
+     * @param id
+     * @return an ArrayList of the customer CustomerFavoriteGenre containing the customer's favorite genre
+     */
     @GetMapping("/favourites/{id}")
     public ArrayList<CustomerFavoriteGenre> customerFavoriteGenres(@PathVariable int id){
        return service.customerFavoriteGenres(id);
     }
+
+    /**
+     *
+     * @param id
+     * @return Boolean if deleting Customer object is successful or not
+     */
     @DeleteMapping("/{id}")
     public Boolean deleteCustomerById(@PathVariable int id){
         return service.deleteCustomerById(id);
